e86a7a0f51e01b6e1fef1e2c0bfdad4a65dff2c6
samba914 <sambadiarra2000@gmail.com>
2023-05-22 00:06:51 +0200

    test

--- composant_cle/test.py
+++ composant_cle/test.py
@@ -1,6 +1,13 @@
-import composant_cle
+import cle_component
 
-macle = composant_cle.cle()
+# Créez une instance de la classe Cle
+macle = cle_component.Cle()
+
+# Initialisez avec une clé privée
 macle.initialize("4b8e29b9b0dddd58a709edba7d6df6c07ebdaf5653e325114bc5318c238f87f0")
-macle.getPrivateKey()
-macle.getPublicKey()
+
+# Récupérez la clé privée
+print(macle.getPrivateKey())
+
+# Récupérez la clé publique
+print(macle.getPublicKey())
